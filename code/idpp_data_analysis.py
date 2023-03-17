ea97c82ab90f235b2f4c9ac2c63c7f9e00be8d8f
Silvador386 <silvador.386@atlas.cz>
2023-03-17 20:51:50 +0100

    Added method to merge all dataset subparts.

--- code/idpp_data_analysis.py
+++ code/idpp_data_analysis.py
@@ -66,12 +66,7 @@ print(unique_patients)
 
 def merge_dfs_complete(dfs):
 
-    # def change_df(orig_df, id_feat, other_feature):
-    #     orig_df = orig_df.sort_values(by=[id_feat, other_feature])
-    #     unique_ids = orig_df[id_feat].unique()
-    #     for
-
-    def change_ts_column_to_list(orig_df, id_feat, other_feature):
+    def group_ts_column_to_list(orig_df, id_feat, other_feature):
         unique_ids = orig_df[id_feat].unique()
         grouped_ts_column_as_list = orig_df.groupby(id_feat)[other_feature].apply(lambda x: x.values).values.tolist()
         new_df = pd.DataFrame.from_dict({id_feat: unique_ids,
@@ -79,18 +74,19 @@ def merge_dfs_complete(dfs):
         new_df = new_df.set_index(id_feat)
         return new_df
 
-    def change_ts_column_select_first(orig_df, id_feat, other_feature):
+    def group_ts_column_select_first(orig_df, id_feat, other_feature):
         unique_ids = orig_df[id_feat].unique()
         first_values = orig_df.groupby(id_feat)[other_feature].apply(lambda x: x.values[0]).values
+
         new_df = pd.DataFrame.from_dict({id_feat: unique_ids,
                                          other_feature: first_values})
         new_df = new_df.set_index(id_feat)
         return new_df
 
-    def group_df_by_patient_id(orig_df, id_feat, time_series_feats, one_occurrence_feats, sort_by_this_feat):
+    def group_ts_df_by_id(orig_df, id_feat, time_series_feats, one_occurrence_feats, sort_by_this_feat):
         orig_df = orig_df.sort_values(by=[ID_FEAT, sort_by_this_feat])
-        ts_dfs = [change_ts_column_to_list(orig_df, id_feat, ts_feat) for ts_feat in time_series_feats]
-        oo_dfs = [change_ts_column_select_first(orig_df, id_feat, oo_feat) for oo_feat in one_occurrence_feats]
+        ts_dfs = [group_ts_column_to_list(orig_df, id_feat, ts_feat) for ts_feat in time_series_feats]
+        oo_dfs = [group_ts_column_select_first(orig_df, id_feat, oo_feat) for oo_feat in one_occurrence_feats]
 
         out_df = pd.concat([*oo_dfs, *ts_dfs], axis=1)
         out_df.reset_index(names=id_feat, inplace=True)
@@ -103,36 +99,34 @@ def merge_dfs_complete(dfs):
     ts_feats = ["delta_relapse_time0"]
     oo_feats = ["centre"]
     sort_by_this_feat = "delta_relapse_time0"
-    relapses_df = group_df_by_patient_id(relapses_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
+    relapses_df = group_ts_df_by_id(relapses_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
+
+    edss_df = dfs["datasetA_train-edss"]
+    ts_feats = ["edss_as_evaluated_by_clinician", "delta_edss_time0"]
+    sort_by_this_feat = "delta_edss_time0"
+    edss_df = group_ts_df_by_id(edss_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
     ms_type_df = dfs["datasetA_train-ms-type"]
     ts_feats = ["multiple_sclerosis_type", "delta_observation_time0"]
     sort_by_this_feat = "delta_observation_time0"
-    ms_type_df = group_df_by_patient_id(ms_type_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
+    ms_type_df = group_ts_df_by_id(ms_type_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
-    #     mri_df = dfs["datasetA_train-mri.csv"]
-    #     ts_feats = ["mri_area_label", "lesions_T1", "lesions_T1_gadolinium", "number_of_lesions_T1_gadolinium",
-    #                 "new_or_enlarged_lesions_T2", "number_of_new_or_enlarged_lesions_T2", "lesions_T2", "number_of_total_lesions_T2", "delta_mri_time0"]
-    #     oo_feats = ["centre"]
-    #     mri_df = transpose_df_by_uniques(mri_df, ID_FEAT, ts_feats, oo_feats)
+    evoked_p_df = dfs["datasetA_train-evoked-potentials"]
+    ts_feats = ["altered_potential", "potential_value", "location", "delta_evoked_potential_time0"]
+    oo_feats = ["centre"]
+    sort_by_this_feat = "delta_evoked_potential_time0"
+    evoked_p_df = group_ts_df_by_id(evoked_p_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
-    #     evoked_p_df = dfs["datasetA_train-evoked-potentials.csv"]
-    #     ts_feats = ["altered_potential", "potential_value", "location", "delta_evoked_potential_time0"]
-    #     oo_feats = ["centre"]
-    #     evoked_p_df = transpose_df_by_uniques(evoked_p_df, ID_FEAT, ts_feats, oo_feats)
+    mri_df = dfs["datasetA_train-mri"]
+    ts_feats = ["mri_area_label", "lesions_T1", "lesions_T1_gadolinium", "number_of_lesions_T1_gadolinium",
+                "new_or_enlarged_lesions_T2", "number_of_new_or_enlarged_lesions_T2", "lesions_T2", "number_of_total_lesions_T2", "delta_mri_time0"]
+    sort_by_this_feat = "delta_mri_time0"
+    mri_df = group_ts_df_by_id(mri_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
-    #     edss_df = dfs["datasetA_train-edss.csv"]
-    #     ts_feats = ["edss_as_evaluated_by_clinician", "delta_edss_time0"]
-    #     oo_feats = ["centre"]
-    #     edss_df = transpose_df_by_uniques(edss_df, ID_FEAT, ts_feats, oo_feats)
+    grouped_dfs = [relapses_df, ms_type_df, mri_df, evoked_p_df, edss_df]
+    for df in grouped_dfs:
+        merged_df = pd.merge(merged_df, df, on=[ID_FEAT, "centre"], how="outer")
 
-    transposed_dfs = [relapses_df, ms_type_df]
-    merged_df = pd.concat([merged_df, *transposed_dfs], axis=1)
-    # print(relapses_df.columns.to_series().groupby(relapses_df.dtypes).groups)
-    # print(merged_df.columns.to_series().groupby(merged_df.dtypes).groups)
-    # merged_df = pd.merge(merged_df, relapses_df, on=ID_FEAT, how="outer")
-
-    merged_df = merged_df.loc[:, ~merged_df.columns.duplicated()].copy()  # Removes duplicate patient_id, centre
     return merged_df
 
 
