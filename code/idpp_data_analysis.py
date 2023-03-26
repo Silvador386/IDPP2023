9d0db31237e41636f7a99571ae13ca8aeea9fc87
Silvador386 <silvador.386@atlas.cz>
2023-03-26 13:54:37 +0200

    Worked on data preprocessing, changed input to support different datasets.

--- code/idpp_data_analysis.py
+++ code/idpp_data_analysis.py
@@ -47,7 +47,8 @@ plot_params = dict(
 )
 
 
-DATASET_DIR = "../data/datasetA_train"
+DATASET = "datasetA"
+DATASET_DIR = f"../data/{DATASET}_train"
 ID_FEAT = "patient_id"
 
 pd.set_option('display.max_rows', None)
@@ -64,8 +65,7 @@ def filenames_in_folder(dir_path):
 
 def read_dfs(dir_path):
     file_names = filenames_in_folder(dir_path)
-    dfs = {file_name.removesuffix(".csv"): pd.read_csv(os.path.join(dir_path, file_name)) for file_name in file_names if
-           file_name.endswith("csv")}
+    dfs = {file_name.removesuffix(".csv"): pd.read_csv(os.path.join(dir_path, file_name)) for file_name in file_names if file_name.endswith("csv")}
     return dfs
 
 
@@ -79,11 +79,7 @@ def dfs_unique_patients(dfs):
     return pd.DataFrame.from_dict(out, orient="index", columns=["Unique patients"])
 
 
-unique_patients = dfs_unique_patients(dfs)
-print(unique_patients)
-
-
-def merge_dfs_complete(dfs):
+def merge_dfs_complete(dfs, dataset):
 
     def group_ts_column_to_list(orig_df, id_feat, other_feature):
         unique_ids = orig_df[id_feat].unique()
@@ -111,36 +107,40 @@ def merge_dfs_complete(dfs):
         out_df.reset_index(names=id_feat, inplace=True)
         return out_df
 
-    merged_df = pd.merge(dfs["datasetA_train-static-vars"], dfs["datasetA_train-outcomes"],
+    merged_df = pd.merge(dfs[f"{dataset}_train-static-vars"], dfs[f"{dataset}_train-outcomes"],
                          on="patient_id", how="outer")
 
-    relapses_df = dfs["datasetA_train-relapses"]
+    relapses_df = dfs[f"{dataset}_train-relapses"]
     ts_feats = ["delta_relapse_time0"]
     oo_feats = ["centre"]
     sort_by_this_feat = "delta_relapse_time0"
     relapses_df = group_ts_df_by_id(relapses_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
-    edss_df = dfs["datasetA_train-edss"]
-    ts_feats = ["edss_as_evaluated_by_clinician", "delta_edss_time0"]
-    sort_by_this_feat = "delta_edss_time0"
-    edss_df = group_ts_df_by_id(edss_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
-
-    ms_type_df = dfs["datasetA_train-ms-type"]
+    ms_type_df = dfs[f"{dataset}_train-ms-type"]
     ts_feats = ["multiple_sclerosis_type", "delta_observation_time0"]
+    oo_feats = ["centre"]
     sort_by_this_feat = "delta_observation_time0"
     ms_type_df = group_ts_df_by_id(ms_type_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
-    evoked_p_df = dfs["datasetA_train-evoked-potentials"]
+    mri_df = dfs[f"{dataset}_train-mri"]
+    ts_feats = ["mri_area_label", "lesions_T1", "lesions_T1_gadolinium", "number_of_lesions_T1_gadolinium",
+                "new_or_enlarged_lesions_T2", "number_of_new_or_enlarged_lesions_T2", "lesions_T2", "number_of_total_lesions_T2", "delta_mri_time0"]
+    oo_feats = ["centre"]
+    sort_by_this_feat = "delta_mri_time0"
+    mri_df = group_ts_df_by_id(mri_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
+
+    evoked_p_df = dfs[f"{dataset}_train-evoked-potentials"]
     ts_feats = ["altered_potential", "potential_value", "location", "delta_evoked_potential_time0"]
     oo_feats = ["centre"]
     sort_by_this_feat = "delta_evoked_potential_time0"
     evoked_p_df = group_ts_df_by_id(evoked_p_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
-    mri_df = dfs["datasetA_train-mri"]
-    ts_feats = ["mri_area_label", "lesions_T1", "lesions_T1_gadolinium", "number_of_lesions_T1_gadolinium",
-                "new_or_enlarged_lesions_T2", "number_of_new_or_enlarged_lesions_T2", "lesions_T2", "number_of_total_lesions_T2", "delta_mri_time0"]
-    sort_by_this_feat = "delta_mri_time0"
-    mri_df = group_ts_df_by_id(mri_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
+
+    edss_df = dfs[f"{dataset}_train-edss"]
+    ts_feats = ["edss_as_evaluated_by_clinician", "delta_edss_time0"]
+    oo_feats = ["centre"]
+    sort_by_this_feat = "delta_edss_time0"
+    edss_df = group_ts_df_by_id(edss_df, ID_FEAT, ts_feats, oo_feats, sort_by_this_feat)
 
     grouped_dfs = [edss_df, relapses_df, ms_type_df, evoked_p_df, mri_df]
     for df in grouped_dfs:
@@ -150,28 +150,75 @@ def merge_dfs_complete(dfs):
 
 
 dfs = read_dfs(DATASET_DIR)
-merged_df = merge_dfs_complete(dfs)
+merged_df = merge_dfs_complete(dfs, DATASET)
+
+
+def encode_and_bind(original_dataframe, feature_to_encode):
+    dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
+    res = pd.concat([original_dataframe, dummies], axis=1)
+    return(res)
+
+
+def df_ts_func(df, feature, func, func_name):
+    calculated_values = []
+    for ts in df[feature]:
+        if ts is None:
+            calculated_values.append(None)
+        else:
+            calculated_values.append(func(ts))
+    out = {f"{feature}_{func_name}": calculated_values}
+    return pd.DataFrame(out)
+
+
+
+def preprocess(merged_df):
+    supported_funcs = {"len": len, "min": np.min, "max": np.max, "sum": np.sum, "avg": np.average}
+    merged_df = merged_df.copy()
+    # sex_map = {"male": 1, "female": 2}
+    # merged_df["sex"] = merged_df["sex"].map(sex_map)
+    #
+    # residence_map = {"Towns": 1, "Rural Area": 2, "Cities": 3}
+    # merged_df["residence_classification"] = merged_df["residence_classification"].map(residence_map)
+    #
+    # ethnicity_map = {"Caucasian": 1, np.nan: 0}
+    # merged_df["ethnicity"] = merged_df["ethnicity"].map(ethnicity_map)
+    #
+    # centre_map = {"pavia": 1, "turin": 2}
+    # merged_df["ethnicity"] = merged_df["ethnicity"].map(centre_map)
+    # bool_features = ['ms_in_pediatric_age', 'spinal_cord_symptom', 'brainstem_symptom',
+    #                  'eye_symptom', 'supratentorial_symptom']
+    # for bool_feat in bool_features:
+    #     merged_df[bool_feat] = merged_df[bool_feat].astype(np.int64)
+    feature = "edss_as_evaluated_by_clinician"
+    created_dfs = [df_ts_func(merged_df, feature, func, name) for name, func in supported_funcs.items()]
+    merged_df = pd.concat([merged_df, *created_dfs], axis=1)
+
+    return merged_df
+
 
 merged_df = merged_df.replace({np.nan: None})
+encode_and_bind(merged_df, "sex")
 
+merged_df = preprocess(merged_df)
 
-outcome, o_time = merged_df["outcome_occurred"], merged_df["outcome_time"]
-ts_feats = ['edss_as_evaluated_by_clinician', 'delta_edss_time0', 'delta_relapse_time0', 'delta_observation_time0', 'delta_evoked_potential_time0', 'delta_mri_time0']
-for feat in ts_feats:
-    fig, axes = plt.subplots(1, 2)
-    axes = axes.flat
-    X = merged_df[feat]
-    for x, out, ot in zip(X, outcome, o_time):
-        if x is None:
-            continue
-        else:
-            alpha = np.interp(len(x), [0, 30], [1, 0])
-            plot_params = dict(color="r" if out == 1 else "0.5",
-                               linestyle="--" if out == 1 else "-")
-            axes[0].plot(x, alpha=alpha, **plot_params)
-            axes[0].set(xlabel="Number of entries", ylabel=f"{feat}")
-
-            alpha = np.interp(ot, [0, 15], [0, 1])
-            axes[1].plot(x, alpha=alpha, **plot_params)
-
-    plt.show()
+print()
+# outcome, o_time = merged_df["outcome_occurred"], merged_df["outcome_time"]
+# ts_feats = ['edss_as_evaluated_by_clinician', 'delta_edss_time0', 'delta_relapse_time0', 'delta_observation_time0', 'delta_evoked_potential_time0', 'delta_mri_time0']
+# for feat in ts_feats:
+#     fig, axes = plt.subplots(1, 2)
+#     axes = axes.flat
+#     X = merged_df[feat]
+#     for x, out, ot in zip(X, outcome, o_time):
+#         if x is None:
+#             continue
+#         else:
+#             alpha = np.interp(len(x), [0, 30], [1, 0])
+#             plot_params = dict(color="r" if out == 1 else "0.5",
+#                                linestyle="--" if out == 1 else "-")
+#             axes[0].plot(x, alpha=alpha, **plot_params)
+#             axes[0].set(xlabel="Number of entries", ylabel=f"{feat}")
+#
+#             alpha = np.interp(ot, [0, 15], [0, 1])
+#             axes[1].plot(x, alpha=alpha, **plot_params)
+#
+#     plt.show()
